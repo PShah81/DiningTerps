186f9caf730208639f3bd5b444d979055837ea7e
Pranav Shah <99997224+PShah81@users.noreply.github.com>
2023-08-21 21:42:14 -0400

    Bug Fix. Favorites section no longer has rounded corners

--- Client/HelperComponents/Food.js
+++ Client/HelperComponents/Food.js
@@ -7,7 +7,7 @@ function Food(props)
 {
     return(
         <View style={{...styles.foodContainer, borderBottomWidth: props.lastItem? verticalScale(0.5): 0, 
-            backgroundColor: props.mode === "Menu" ? null : colorObject["grey"]["7"]}}>
+            backgroundColor: props.mode === "Database" ? colorObject["grey"]["7"] : null}}>
             <TouchableOpacity onPress={() => {props.onItemClick(props.foodData, props.foodName)}} style={styles.foodButton}>
                 <CustomText style= {{...styles.foodName, fontSize: props.foodName.length > 45 ? moderateScale(10) : moderateScale(14)}} text={props.foodName}/> 
                 <View style={styles.foodAllergies}>
